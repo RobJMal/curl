426561ac619e0f38e1207c975c4966262e48a5e7
RobJMal <robjmal@gmail.com>
2024-05-01 22:06:04 +0200

    Added automatic checking of dmc environment and task to reduce additional arguments needed

--- visualize.py
+++ visualize.py
@@ -7,14 +7,14 @@ import pandas as pd
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--environment', type=str, help='Name of DMC environment (ex. fish, walker)', required=True)
-parser.add_argument('--task', type=str, help='Name of task for environment', required=True)
 parser.add_argument('--path_to_data', type=str, help='Path to file to plot data from (eval or train)', required=True)
 parser.add_argument('--ema_span', type=int, default=20, help='Span for Moving Exponential Average')
 args = parser.parse_args()
 
 # Path to the log file
 log_file_path = args.path_to_data
+log_directory_tokens = log_file_path.split('/')[-2].split('-')    # Log directory is always 2nd to last token
+dmc_environment, dmc_task = log_directory_tokens[0], log_directory_tokens[1]
 
 collected_data_type = ''
 if 'eval' in log_file_path:
@@ -47,7 +47,7 @@ mean_episode_rewards_ema = pd.Series(mean_episode_rewards).ewm(span=ema_span, ad
 plt.figure(figsize=(10, 5))
 plt.plot(steps, episode_rewards, label='Episode Reward', marker='o', color='blue')
 plt.plot(steps, episode_rewards_ema, label="Episode Rewards EMA", linestyle='-', color='red')
-plot_title = f'{collected_data_type} Episode Reward (CURL {args.environment}-{args.task})'
+plot_title = f'{collected_data_type} Episode Reward (CURL {dmc_environment}-{dmc_task})'
 plt.title(plot_title)
 plt.xlabel('Step')
 plt.ylabel('Reward')
